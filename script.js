d8cdad95b3f686f022d30025b2ff16fbea16ec1a
guilhermebernardini-cpu <guilherme.bernardini@aluno.lsb.com.br>
2025-11-04 20:32:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function calcular() {
+   // passo 1 - oboter o valor que o usuario digitou no INPUT
+   // passo 2 - realizar o calculo
+   // passo 3 - alterar o conteúdo do que tem na tag SPAN
+
+   let valorbr = document.getElementById("valorbr").value;
+   fetch("https://api.frankfurter.dev/v1/latest?base=USD&to=BRL")
+      .then(res => res.json())
+      .then(res2 => {
+         let valorus = valorbr * res2.rates.BRL;
+         document.getElementById("valor").innerHTML = valorus;
+         document.getElementById("cotacao").innerHTML = res2.rates.BRL;
+      })
+      .catch(err => {
+         alert("Erro ao receber cotacao do dolar!")
+
+      })
+}
